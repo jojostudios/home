8561496b69ee53c3c0a30698262b506c82484d2a
jojostudios <charlesmitchell@protonmail.com>
2016-11-02 14:15:26 -0700

    Add files via upload

---
+++ Jojo Studio/php/index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<style>
+body {
+	text-align:center;
+	vertical-align:central;
+	font-family: reenie-beanie, sans-serif;
+	font-style: normal;
+	font-weight: 500;
+}
+</style>
+<script src="https://use.typekit.net/hqy7ydj.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+
+<title>JoJo_Studio</title>
+</head>
+
+<body>
+
+Coming Soon
+
+
+<div class="contact">
+	<p>Inquiries</p>
+	<label><input type="text" class="contact_text" name="Name" placeholder="Name"></label>
+    <label><input type="text" class="contact_text" name="" placeholder=""></label>
+    <label><input type="text" class="contact_text" name="" placeholder=""></label>
+</div>
+
+<!--
+inquiries box
+projects
+photos
+newsletter pop up
+
+
+-->
+
+</body>
+</html>
